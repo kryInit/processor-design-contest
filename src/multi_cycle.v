471389c003c0f71cb462c52d5d0ff7ae6ad13bcc
kryInit <kryinit@gmail.com>
2023-05-22 21:22:51 +0900

    命令格納用メモリも同期メモリに変更

--- src/multi_cycle.v
+++ src/multi_cycle.v
@@ -51,15 +51,23 @@ module m_proc14 (w_clk, w_ce, w_led);
     input  wire w_clk, w_ce;
     output wire [31:0] w_led;
 
+    // stage 0
     m_IF IF(w_clk, w_ce, ID.do_speculative_branch, ID.branch_dest_addr, MEM.do_branch, MEM.branch_dest_addr);
+
+    // stage 1
     m_ID ID(w_clk, w_ce, MEM.do_branch, IF.pc, IF.instr, WB.reg_dest, WB.writing_value);
+
+    // stage 2
     m_EX EX(
         w_clk, w_ce, MEM.do_branch, ID.pc, ID.instr, ID.imm,
         ID.reg_source1, ID.reg_source2, ID.reg_data1, ID.reg_data2, ID.reg_dest,
         MEM.reg_dest, WB.reg_dest, MEM.calced_value, WB.writing_value
     );
 
+    // stage 3
     m_MEM MEM(w_clk, w_ce, EX.pc, EX.instr, EX.imm, EX.calced_value, EX.reg_source1, EX.reg_source2, EX.reg_data2, EX.reg_dest, EX.lhs_operand, EX.rhs_operand );
+
+    // stage 4
     m_WB WB(w_clk, w_ce, MEM.pc, MEM.instr, MEM.calced_value, MEM.memory_data, MEM.reg_dest);
 
     reg [31:0] r_led = 0;
@@ -75,8 +83,8 @@ module m_IF ( clk, ce, ID_do_speculative_branch, ID_branch_dest_addr, EX_do_bran
     reg [31:0] pc = 0;
     wire [31:0] instr;
 
-    //                clk, addr,     we,   input, output
-    m_amemory m_imem (clk, pc[13:2], 1'd0, 32'd0, instr);
+    //                            clk, addr,     we,   input, output
+    m_instr_memory instr_memory(clk, pc[13:2], 1'd0, 32'd0, instr);
 
     always @(posedge clk) #5 if(ce && instr != 32'h000f0033) pc <= EX_do_branch ? EX_branch_dest_addr : ID_do_speculative_branch ? ID_branch_dest_addr : pc+4;
 endmodule
@@ -197,7 +205,8 @@ module m_MEM ( clk, ce, EX_pc, EX_instr, EX_imm, EX_calced_value, EX_reg_source1
     wire [4:0] short_opcode = instr[6:2];
     wire is_store_instr = short_opcode == 5'b01000;
     wire [31:0] memory_data;
-    m_memory m_dmem (clk, calced_value[13:2], is_store_instr, reg_data2, memory_data);
+
+    m_data_memory data_memory(clk, calced_value[13:2], is_store_instr, reg_data2, memory_data);
 endmodule
 
 module m_WB ( clk, ce, MEM_pc, MEM_instr, MEM_calced_value, MEM_memory_data, MEM_reg_dest );
@@ -223,18 +232,7 @@ module m_WB ( clk, ce, MEM_pc, MEM_instr, MEM_calced_value, MEM_memory_data, MEM
     wire [31:0] writing_value = is_load_instr ? memory_data : calced_value;
 endmodule
 
-module m_amemory (w_clk, w_addr, w_we, w_din, w_dout); // asynchronous memory
-  input  wire w_clk, w_we;
-  input  wire [11:0] w_addr;
-  input  wire [31:0] w_din;
-  output wire [31:0] w_dout;
-  reg [31:0] 	     cm_ram [0:4095]; // 4K word (4096 x 32bit) memory
-  always @(posedge w_clk) if (w_we) cm_ram[w_addr] <= w_din;
-  assign #20 w_dout = cm_ram[w_addr];
-`include "../inputs/program6.txt" // [include]
-endmodule
-
-module m_memory (w_clk, w_addr, w_we, w_din, r_dout); // synchronous memory
+module m_instr_memory (w_clk, w_addr, w_we, w_din, r_dout); // synchronous memory
   input  wire w_clk, w_we;
   input  wire [11:0] w_addr;
   input  wire [31:0] w_din;
@@ -243,6 +241,16 @@ module m_memory (w_clk, w_addr, w_we, w_din, r_dout); // synchronous memory
   always @(posedge w_clk) if (w_we) cm_ram[w_addr] <= w_din;
   always @(posedge w_clk) r_dout <= cm_ram[w_addr];
 `include "../inputs/program6.txt" // [include]
+endmodule
+
+module m_data_memory (w_clk, w_addr, w_we, w_din, r_dout); // synchronous memory
+  input  wire w_clk, w_we;
+  input  wire [11:0] w_addr;
+  input  wire [31:0] w_din;
+  output reg  [31:0] r_dout;
+  reg [31:0]         cm_ram [0:4095]; // 4K word (4096 x 32bit) memory
+  always @(posedge w_clk) if (w_we) cm_ram[w_addr] <= w_din;
+  always @(posedge w_clk) r_dout <= cm_ram[w_addr];
 endmodule
 
 module m_immgen (w_i, r_imm); // module immediate generator
